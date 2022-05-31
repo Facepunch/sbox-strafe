fb24fd2086891622c9bdf62d199637bdae9bb465
Jake Evans <jacob.ee92@gmail.com>
2022-05-31 09:00:55 -0600

    Add strafe_player_visibility convar (0 = fade, 1 = always, 2 = never)

---
+++ code/Players/StrafePlayer.Opacity.cs
@@ -0,0 +1,43 @@
+﻿
+using Sandbox;
+
+using Strafe.Utility;
+
+namespace Strafe.Players;
+
+internal partial class StrafePlayer
+{
+
+	public enum PlayerVisibility
+	{
+		Fade = 0,
+		Always = 1,
+		Never = 2
+	}
+
+	[ConVar.Client( "strafe_player_visibility" )]
+	public static PlayerVisibility Visibility { get; set; } = PlayerVisibility.Fade;
+
+	[Event.Frame]
+	private void UpdateRenderAlpha() => this.SetRenderAlphaRecursive( GetRenderAlpha() );
+
+	private float GetRenderAlpha()
+	{
+		if ( Visibility == PlayerVisibility.Always ) return 1f;
+		if ( Visibility == PlayerVisibility.Never ) return 0;
+		if ( Visibility == PlayerVisibility.Fade )
+		{
+			if ( !Local.Pawn.IsValid() ) return 1f;
+
+			const float MaxRenderDistance = 900f;
+			var dist = Local.Pawn.Position.Distance( Position );
+			var a = 1f - dist.LerpInverse( MaxRenderDistance, MaxRenderDistance * .1f );
+			a = Easing.EaseOut( a );
+
+			return a;
+		}
+
+		return 1f;
+	}
+
+}
