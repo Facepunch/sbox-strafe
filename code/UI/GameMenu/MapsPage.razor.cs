aa1add9ba8ec392354985165efc4ff41839abfa1
cr4yz <jacob.ee92@gmail.com>
2023-06-06 20:04:37 -0600

    Maps page

---
+++ code/UI/GameMenu/MapsPage.razor.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Linq;
+
+namespace Strafe.UI;
+
+public partial class MapsPage : Panel
+{
+
+	List<Package> Maps = new();
+
+	protected override void OnAfterTreeRender( bool firstTime )
+	{
+		base.OnAfterTreeRender( firstTime );
+
+		if ( firstTime )
+			Refresh();
+	}
+
+	async void Refresh()
+	{
+		var mapidents = await StrafeGame.GetAvailableMaps();
+
+		foreach ( var ident in mapidents.Distinct() )
+		{
+			var pkg = await Package.Fetch( ident, true );
+			if ( pkg == null ) continue;
+			Maps.Add( pkg );
+		}
+	}
+
+}
