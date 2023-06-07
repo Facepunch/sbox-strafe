062a52e954363576ed1ebd1f214d57aac15b770b
cr4yz <jacob.ee92@gmail.com>
2023-06-06 18:09:14 -0600

    Simple server filter

--- code/UI/GameMenu/ServersPage.razor.cs
+++ code/UI/GameMenu/ServersPage.razor.cs
@@ -6,6 +6,28 @@ public partial class ServersPage : Panel
 
 	ServerList List;
 
+	bool showEmpty = true;
+	bool ShowEmpty
+	{
+		get => showEmpty;
+		set
+		{
+			showEmpty = value;
+			StateHasChanged();
+		}
+	}
+
+	bool whitelistedOnly = false;
+	bool WhitelistedOnly
+	{
+		get => whitelistedOnly;
+		set
+		{
+			whitelistedOnly = value;
+			StateHasChanged();
+		}
+	}
+
 	protected override void OnAfterTreeRender( bool firstTime )
 	{
 		base.OnAfterTreeRender( firstTime );
@@ -27,6 +49,22 @@ public partial class ServersPage : Panel
 			await Task.Delay( 100 );
 
 		StateHasChanged();
+	}
+
+	bool Filter( ServerList.Entry e )
+	{
+		if ( !showEmpty && e.Players == 0 )
+			return false;
+
+		if ( whitelistedOnly && !IsWhitelisted( e.SteamId ) )
+			return false;
+
+		return true;
+	}
+
+	bool IsWhitelisted( ulong steamid )
+	{
+		return false;
 	}
 
 }
