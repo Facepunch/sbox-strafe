216d8d74b9d39c04f9417c8cc809cfa5b6545e4e
cr4yz <jacob.ee92@gmail.com>
2023-06-08 12:31:31 -0600

    Can sort maps by various parameters

--- code/UI/GameMenu/MapsPage.razor.cs
+++ code/UI/GameMenu/MapsPage.razor.cs
@@ -4,11 +4,21 @@ namespace Strafe.UI;
 public partial class MapsPage : Panel
 {
 
+	enum SortMode 
+	{
+		None,
+		HoursPlayed,
+		MostUniquePlayers,
+		Newest,
+		Oldest
+	}
+
 	List<Package> Maps = new();
 	DropDown MapTypeDropDown;
 
 	MapTypes MapTypeFilter { get; set; } = MapTypes.None;
 	MapDifficulties MapDifficultyFilter { get; set; } = MapDifficulties.None;
+	SortMode SortModeFilter { get; set; } = SortMode.Newest;
 
 	protected override void OnAfterTreeRender( bool firstTime )
 	{
@@ -30,6 +40,26 @@ public partial class MapsPage : Panel
 			if ( pkg == null ) continue;
 			Maps.Add( pkg );
 		}
+	}
+
+	IEnumerable<Package> Sort( List<Package> stuff )
+	{
+		if ( SortModeFilter == SortMode.None )
+			return stuff;
+
+		if ( SortModeFilter == SortMode.HoursPlayed )
+			return stuff.OrderByDescending( x => x.Usage.Total.Seconds );
+
+		if ( SortModeFilter == SortMode.MostUniquePlayers )
+			return stuff.OrderByDescending( x => x.Usage.Total.Users );
+
+		if ( SortModeFilter == SortMode.Newest )
+			return stuff.OrderByDescending( x => x.Created );
+
+		if ( SortModeFilter == SortMode.Oldest )
+			return stuff.OrderBy( x => x.Created );
+
+		return stuff;
 	}
 
 	bool Filter( Package pkg )
