15f708a5aaebd11976b26c7c7c76578b8e9de53c
cr4yz <jacob.ee92@gmail.com>
2023-06-12 11:11:56 -0600

    Misc settings page

---
+++ code/StrafeClientSettings.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Strafe;
+
+internal static class StrafeClientSettings
+{
+
+	public static StrafeSettings Settings { get; private set; }
+
+	static StrafeClientSettings()
+	{
+		Settings = Cookie.Get<StrafeSettings>( "strafe_client_settings", new() );
+	}
+
+	public static void Save()
+	{
+		Cookie.Set( "strafe_client_settings", Settings );
+	}
+
+	public static void ResetDefaults()
+	{
+		Settings = new();
+		Save();
+	}
+
+	public class StrafeSettings
+	{
+		public bool SnailTrail { get; set; }
+		public bool ShowInput { get; set; }
+		public StrafePlayer.PlayerVisibility PlayerVisibility { get; set; } = StrafePlayer.PlayerVisibility.Fade;
+		public ViewModelPositions ViewModelPosition { get; set; } = ViewModelPositions.Right;
+	}
+
+}
