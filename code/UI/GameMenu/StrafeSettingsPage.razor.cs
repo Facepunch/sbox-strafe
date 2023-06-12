42481f372571a9c9f9d24b48af58596430be6afe
cr4yz <jacob.ee92@gmail.com>
2023-06-12 10:32:44 -0600

    Setting save/restore

--- code/UI/GameMenu/StrafeSettingsPage.razor.cs
+++ code/UI/GameMenu/StrafeSettingsPage.razor.cs
@@ -4,6 +4,12 @@ namespace Strafe.UI;
 public partial class StrafeSettingsPage : NavigatorPanel
 {
 
+	protected override void OnAfterTreeRender( bool firstTime )
+	{
+		base.OnAfterTreeRender( firstTime );
 
+		if ( firstTime )
+			Navigate( "/menu/settings/input" );
+	}
 
 }
