89b84b50e42bfc5791e98d80359154bb421673a9
cr4yz <jacob.ee92@gmail.com>
2023-08-24 20:20:05 -0600

    Add ranks page listing the top 50 players

--- code/Api/Backend.Players.cs
+++ code/Api/Backend.Players.cs
@@ -4,6 +4,11 @@ namespace Strafe.Api;
 internal partial class Backend
 {
 
+	public static async Task<List<PlayerData>> FetchRankLeaderboard( int take, int skip )
+	{
+		return await Get<List<PlayerData>>( $"player/fetch/ranks?take={take}&skip={skip}" );
+	}
+
 	public static async Task<List<PlayerData>> FetchCreditsLeaderboard( int take, int skip )
 	{
 		return await Get<List<PlayerData>>( $"player/fetch/credits?take={take}&skip={skip}" );
--- code/Api/Messages/PlayerData.cs
+++ code/Api/Messages/PlayerData.cs
@@ -7,6 +7,7 @@ public class PlayerData
 	public long SteamId { get; set; }
 	public string Name { get; set; }
 	public int Credits { get; set; }
+	public int Points { get; set; }
 	public DateTimeOffset FirstSeen { get; set; }
 	public DateTimeOffset LastSeen { get; set; }
 
